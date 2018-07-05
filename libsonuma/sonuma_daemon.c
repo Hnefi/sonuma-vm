e13313e7d13108ac78f83dbde7007ecc157f81ce
Hnefi <mark.sutherland@epfl.ch>
2018-07-05 13:32:26 +0200

    Testing RMCD

--- libsonuma/sonuma_daemon.c
+++ libsonuma/sonuma_daemon.c
@@ -167,11 +167,6 @@ void rmc_send(rmc_wq_t *wq, char *lbuff_ptr, int lbuff_offset, size_t size, int 
     send_slot->wq_entry_idx = wq_head;
     wq->q[wq_head].buf_addr = (uint64_t)lbuff_ptr;
     wq->q[wq_head].buf_offset = lbuff_offset;
-    /*
-    *(lbuff_ptr + (lbuff_offset+size)) = 's';
-    *(lbuff_ptr + (lbuff_offset+size+1)) = sending_qp;
-    *(lbuff_ptr + (lbuff_offset+size+2)) = slot_idx;
-    */
 #ifdef PRINT_BUFS
     print_cbuf( (char*)lbuff_ptr , size );
 #endif
@@ -200,11 +195,6 @@ void rmc_recv(rmc_wq_t *wq, char* lbuff_ptr,int lbuff_offset,size_t size,int sni
     DLog("[rmc_recv] rmc_recv called.");
 
     while (wq->q[wq_head].valid) {} //wait for WQ head to be ready
-    /*
-    *(lbuff_ptr + (lbuff_offset+size)) = 'g';
-    *(lbuff_ptr + (lbuff_offset+size+1)) = sending_qp;
-    *(lbuff_ptr + (lbuff_offset+size+2)) = slot_idx;
-    */
 #ifdef PRINT_BUFS
     print_cbuf( (char*)lbuff_ptr , size );
 #endif
