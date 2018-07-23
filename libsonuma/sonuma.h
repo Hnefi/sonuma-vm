ec8d4f60051a614467973dc2aa6e27c3c2abd139
Hnefi <mark.sutherland@epfl.ch>
2018-07-23 16:56:23 +0200

    Compilation

--- libsonuma/sonuma.h
+++ libsonuma/sonuma.h
@@ -74,6 +74,40 @@ typedef void (async_handler)(uint8_t tid, wq_entry_t *head, void *owner);
 typedef void (rpc_handler)(uint16_t sending_nid, char* recv_slot, cq_entry_t *head, void *owner);
 typedef void (receiveCallback)(char* rawRecvBufferPtr, rpcArg_t* argPointer);
 
+/* Helper */
+// Shamelessly imported from: https://gist.github.com/ccbrown/9722406#file-dumphex-c
+// - thanks @ccbrown! : https://github.com/ccbrown
+//
+static void DumpHex(const void* data, size_t size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printf("%02X ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printf(" ");
+			if ((i+1) % 16 == 0) {
+				printf("|  %s \n", ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printf(" ");
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printf("   ");
+				}
+				printf("|  %s \n", ascii);
+			}
+		}
+	}
+}
+
+
 /**
  * This func opens connection with kernel driver (KAL).
  */
@@ -391,40 +425,6 @@ static inline void rmc_test_cq_rpc(rmc_cq_t* cq, char** recv_slots, receiveCallb
       }
   } else *sending_nid = -1;
 }
-
-/* Helper */
-// Shamelessly imported from: https://gist.github.com/ccbrown/9722406#file-dumphex-c
-// - thanks @ccbrown! : https://github.com/ccbrown
-//
-static void DumpHex(const void* data, size_t size) {
-	char ascii[17];
-	size_t i, j;
-	ascii[16] = '\0';
-	for (i = 0; i < size; ++i) {
-		printf("%02X ", ((unsigned char*)data)[i]);
-		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-			ascii[i % 16] = ((unsigned char*)data)[i];
-		} else {
-			ascii[i % 16] = '.';
-		}
-		if ((i+1) % 8 == 0 || i+1 == size) {
-			printf(" ");
-			if ((i+1) % 16 == 0) {
-				printf("|  %s \n", ascii);
-			} else if (i+1 == size) {
-				ascii[(i+1) % 16] = '\0';
-				if ((i+1) % 16 <= 8) {
-					printf(" ");
-				}
-				for (j = (i+1) % 16; j < 16; ++j) {
-					printf("   ");
-				}
-				printf("|  %s \n", ascii);
-			}
-		}
-	}
-}
-
 #ifdef __cplusplus
 extern "C" {
 #endif
